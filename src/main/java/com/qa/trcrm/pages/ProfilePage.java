0960372d7abff05cc0881d0f0acf040bbcbbb3ba
sachinsharma263 <sachinsharma263@gmail.com>
2022-12-29 20:36:18 +0530

    updated

--- src/main/java/com/qa/trcrm/pages/ProfilePage.java
+++ src/main/java/com/qa/trcrm/pages/ProfilePage.java
@@ -3,12 +3,19 @@ package com.qa.trcrm.pages;
 import org.openqa.selenium.WebDriver;
 
 public class ProfilePage {
-WebDriver driver;
-	public ProfilePage(WebDriver driver)
-	{
-		this.driver=driver;
+	WebDriver driver;
+
+	public ProfilePage(WebDriver driver) {
+		this.driver = driver;
 	}
+
+	public void local() {
+	}
+
 	public void remote()
 	{}
 	
+
+
+
 }
