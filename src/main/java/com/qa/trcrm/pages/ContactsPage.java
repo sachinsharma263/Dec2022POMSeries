d6367df1e43289ee6a6fff6289ea152d0700013c
sachinsharma263 <sachinsharma263@gmail.com>
2022-12-30 21:27:26 +0530

    updated

--- src/main/java/com/qa/trcrm/pages/ContactsPage.java
+++ src/main/java/com/qa/trcrm/pages/ContactsPage.java
@@ -1,5 +1,63 @@
 package com.qa.trcrm.pages;
 
-public class ContactsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.trcrm.base.BasePage;
+import com.qa.trcrm.pojo.Contacts;
+import com.qa.trcrm.utils.ElementUtil;
+
+public class ContactsPage extends BasePage {
+
+	WebDriver driver;
+	ElementUtil util;
+
+	By contactPageHeader = By.xpath("(//h2[text()])[1]");
+
+	By addPersonButton = By.xpath("//button[@class='hidden-xs hidden-sm btn btn-danger mr5 ng-scope ng-binding']");
+	By name = By.name("name");
+	By emailId = By.id("email0");
+	By saveButton = By.xpath("//button[@class='btn btn-primary btn-large ng-binding']");
+	By personAddedMsg = By.xpath("//span[text()='Person added.']");
+
+	public ContactsPage(WebDriver driver) {
+		this.driver = driver;
+		util = new ElementUtil(driver);
+	}
+
+	public String contactPageTitle() {
+		return util.doGetTitle();
+	}
+
+	public String verifyContactPageHeader() {
+		util.waitForPresenceOfELement(contactPageHeader);
+		return util.doGetText(contactPageHeader);
+	}
+
+	public String addPerson(String firstName, String email) {
+
+		util.waitForPresenceOfELement(addPersonButton);
+		util.doClick(addPersonButton);
+		util.waitForPresenceOfELement(name);
+		util.doActionSenkeys(name, firstName);
+		util.doSendKeys(emailId, email);
+		util.doClick(saveButton);
+
+		util.waitForPresenceOfELement(personAddedMsg);
+		return util.doGetText(personAddedMsg);
+	}
+
+	public String addPerson(Contacts contacts) {
+
+		util.waitForPresenceOfELement(addPersonButton);
+		util.doClick(addPersonButton);
+		util.waitForPresenceOfELement(name);
+		util.doActionSenkeys(name, contacts.getName());
+		util.doSendKeys(emailId, contacts.getEmail());
+		util.doClick(saveButton);
+
+		util.waitForPresenceOfELement(personAddedMsg);
+		return util.doGetText(personAddedMsg);
+	}
 
 }
